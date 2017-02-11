815a5d1ccc9d3307fdd3ffbad6cd14e4be8bb06e
Benjamin Soung <soungbenjamin@gmail.com>
2017-02-11 12:22:52 -0500

    tweaks

---
+++ src/reducers/messageReducer.js
@@ -0,0 +1,22 @@
+import constants from '../constants';
+
+let initialState = {
+
+}
+
+export default (state = initialState, action) => {
+	let updated = Object.assign({}, state);
+
+	switch (action.type) {
+		case constants.MESSAGES_RECEIVED:
+
+			let taskId = action.params.task;
+			updated[taskId] = action.payload
+
+
+			return updated;
+
+		default:
+			return state;
+	}
+}
